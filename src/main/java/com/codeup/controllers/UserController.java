2f0325fa0f01bfa60e794d22ed1cfc2bc20370b9
Amberlee <amberleec12@yahoo.com>
2017-06-27 09:41:03 -0500

    All files were ignored but now are not

--- src/main/java/com/codeup/controllers/UserController.java
+++ src/main/java/com/codeup/controllers/UserController.java
@@ -2,11 +2,35 @@ package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import com.codeup.models.User;
+import com.codeup.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 /**
  * Created by Amberlee on 6/23/17.
  */
 
-@Controller
+
+    @Controller
     public class UserController {
+
+        @Autowired
+        UsersRepository usersDao;
+
+        @Autowired
+        private PasswordEncoder passwordEncoder;
+
+        @PostMapping("/register")
+        public String registerUser(@ModelAttribute User user) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            usersDao.save(user);
+            return "redirect:login";
+        }
     }
+
 
